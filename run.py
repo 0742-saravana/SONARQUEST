5cb27fe97a9f9bcdbf31797fda13dcb00e2ed845
Saro-0742 <imsaravanan2004@gmail.com>
2026-09-02 16:10:50 +0530

    Update comment for clarity in run.py

--- run.py
+++ run.py
@@ -2,5 +2,5 @@ import uvicorn
 
 if __name__ == "__main__":
     print("Starting SONARQUESTV2 Server...")
-    # Run the FastAPI app with Uvicorn programmatically
+    # Run the FastAPI app with Uvicorn programmatically in server
     uvicorn.run("backend.main:app", host="127.0.0.1", port=8000, log_level="info")
